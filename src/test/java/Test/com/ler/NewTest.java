61102d925b2228a8c1cbe0d38fa6fb44307abec5
vipin14 <vipinsaxenatest@gmail.com>
2018-05-02 22:14:30 +0530

    first code

---
+++ src/test/java/Test/com/ler/NewTest.java
@@ -0,0 +1,29 @@
+package Test.com.ler;
+import org.testng.annotations.Test;
+
+import org.openqa.selenium.By;		
+import org.openqa.selenium.WebDriver;		
+import org.openqa.selenium.firefox.FirefoxDriver;		
+import org.testng.Assert;		
+import org.testng.annotations.Test;	
+import org.testng.annotations.BeforeTest;	
+import org.testng.annotations.AfterTest;		
+
+public class NewTest {		
+	    private WebDriver driver;
+		@Test				
+		public void testEasy() {	
+			 	System.out.println("******************Vipin***************");
+			 	Assert.assertEquals("Hello", "Hello");
+		}	
+		@Test
+		public void beforeTest() {	
+			System.out.println("******************Vipin***************");
+		 	Assert.assertEquals("ave", "ave"); 
+		}		
+		@Test
+		public void afterTest() {
+			System.out.println("******************Vipin***************");
+		 	Assert.assertEquals("Hello", "Hello");	
+		}		
+}	
